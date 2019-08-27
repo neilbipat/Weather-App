f4492caf5cb62b4cb98317f4be4683f1abe6703e
mchap <mary.chapman@generalassembl.ly>
2019-08-27 14:20:16 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log('main.js is connected!');
+
+/*
+
+Here's an overview of the steps you'll follow to get your app to work...
+
+STEPS
+
+1. when the page loads
+  - add an event listener to the button
+2. When the button is clicked
+  - grab the input
+  - store the value
+  - make an API request based on the input value
+3. When the API response is returned
+  - grab all the appropriate DOM elements
+  - append the data to the DOM
+
+*/
